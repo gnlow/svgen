9a71441a84c093e599c3ebdf5dfa6da72b7379b8
Gnlow <gnlow@protonmail.com>
2022-09-21 16:12:08 +0900

    Update render.ts

--- src/render.ts
+++ src/render.ts
@@ -1,5 +1,6 @@
 import { renderToString } from "https://esm.sh/react-dom@18.2.0/server"
 
-const { default: Component } = await import(Deno.args[0])
-
-console.log(renderToString(Component()))
+export default async function render(input: string, output: string) {
+    const { default: Component } = await import(input)
+    await Deno.writeTextFile(output, renderToString(Component()))
+}
